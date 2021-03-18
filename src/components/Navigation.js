ed6781f310518fafffdd5bb26e3f695fd5238307
dawiteu <dawid@tararuj.be>
2021-03-18 10:38:25 +0100

    structure ok, header + nav

---
+++ src/components/Navigation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <nav>
+            <a href="###">Dawid Tararuj</a>
+
+            <ul>
+                <li>Portfolio</li>
+                <li>Skills</li>
+                <li>About</li>
+                <li>S. Media</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navigation;
