4a1045a6ddaf30e5e3ae9d39f318478c9a031696
dawiteu <dawid@tararuj.be>
2021-03-18 16:04:10 +0100

    skills

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -3,7 +3,22 @@ import React from 'react';
 const Skills = () => {
     return (
         <section className="content" id="skills">
-            sKillsSS
+            <div class="cont">
+
+                HTML :<i class="fab fa-html5 fa-6x"></i>
+                CSS : <i class="fab fa-css3-alt fa-6x"></i>
+                JS: <i class="fab fa-js-square fa-6x"></i>
+                SASS: <i class="fab fa-sass fa-6x"></i>
+                React : <i class="fab fa-react fa-6x"></i>
+                <br/> 
+                PHP: <i class="fab fa-php fa-6x"></i>
+                MySQL: : <i class="fas fa-database fa-6x"></i>
+                <br/>
+                BASH: <i class="fas fa-terminal fa-6x"></i>
+                GitHUB: <i class="fab fa-github fa-6x"></i>
+                
+                
+            </div>
         </section>
     );
 };
