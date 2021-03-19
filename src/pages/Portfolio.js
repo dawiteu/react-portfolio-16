eccd1d178c98069a22262e9e5941b6362a5615ee
dawiteu <dawid@tararuj.be>
2021-03-19 11:59:30 +0100

    portfolio and skills changements

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import chef from "../assets/imgs/site_chef.png";
 
 const Portfolio = () => {
+    const [images, setImages] = useState([]); 
+    const [img, setImg] = useState({}); 
+
+    const projet = [ 
+        {nom: "Site du chef", img: "site_chef"}
+    ]; 
+
+    projet.map((element, i) => {
+        let updateImages = [...images];
+        //console.log(element.img); 
+
+        import('../assets/imgs/'+element.img+'.png').then(image => setImg({image}) );
+        //updateImages.push()
+        //console.log();
+    });
+
     return (
         <section className="content" id="portfolio">
-            {/* <h1>Portfolio</h1> */}
+            {/* <h1>Portfolio</h1> <img src={chef} alt="marche pas" />*/}
+
+            {}
+
             <div className="cont">
-                <div className="project">
-                    <h4>site du chef // coding 16</h4>
-                    <p></p>
-                </div>
+                {
+                    projet.map((el, i) => {
+                        return (
+                            <div key={i} className='project'> 
+                                <div className="desc">{el.nom}</div> 
+                                <img src={el.img} alt={el.nom} /> 
+                            </div>
+                        )
+                    })
+                }
             </div>
+
+            
         </section>
     );
 };
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -4,20 +4,108 @@ const Skills = () => {
     return (
         <section className="content" id="skills">
             <div className="cont">
+                <div className="row">
+                    <div className="col">
+                        <h3 className="title">Front &#123; </h3>
+                        <div className="compets">
 
-                HTML :<i className="fab fa-html5 fa-6x"></i>
-                CSS : <i className="fab fa-css3-alt fa-6x"></i>
-                JS: <i className="fab fa-js-square fa-6x"></i>
-                SASS: <i className="fab fa-sass fa-6x"></i>
-                React : <i className="fab fa-react fa-6x"></i>
+                            <div className="compet">
+                                <dt><i className="fab fa-html5 fa-3x"></i></dt>
+                                <dd>HTML 5</dd> 
+                            </div>
+                            
+                            <div className="compet">
+                                <dt><i className="fab fa-css3-alt fa-3x"></i></dt>
+                                <dd>CSS 3</dd>
+                            </div>
+                            
+                            <div className="compet">
+                                <dt><i className="fab fa-js-square fa-3x"></i></dt>
+                                <dd>JS</dd>
+                            </div>
+
+                            <div className="compet">
+                                <dt><i className="fab fa-sass fa-3x"></i></dt>
+                                <dd>Sass/Scss</dd>
+                            </div>
+
+                            <div className="compet">
+                                <dt><i className="fab fa-react fa-3x"></i></dt>
+                                <dd>ReactJS</dd>
+                            </div>
+
+                        </div>
+                        <h3 className="title">&#125; </h3>
+                    </div>
+                    <div className="col">
+                        <h3 className="title">Back</h3>
+                        <div className="compets">
+
+                            <div className="compet">
+                                <dt><i className="fab fa-php fa-3x"></i></dt>
+                                <dd>PHP</dd> 
+                            </div>
+
+                            <div className="compet">
+                                <dt><i className="fas fa-database fa-3x"></i></dt>
+                                <dd>MySQL</dd> 
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div className="col">
+                        <h3 className="title">+</h3>
+
+                        <div className="compets">
+
+                            <div className="compet">
+                                <dt><i className="fas fa-terminal fa-3x"></i></dt>
+                                <dd>Bash</dd> 
+                            </div>
+
+                            <div className="compet">
+                                <dt><i className="fab fa-github fa-3x"></i></dt>
+                                <dd>GitHub</dd> 
+                            </div>
+
+                            <div className="compet">
+                                <dt><i className="fas fa-images fa-3x"></i></dt>
+                                <dd>Photoshop</dd> 
+                            </div>
+
+                            <div className="compet">
+                                <dt><i className="fas fa-images fa-3x"></i></dt>
+                                <dd>Illustrator</dd> 
+                            </div>
+
+                            <div className="compet">
+                                <dt><i className="fas fa-images fa-3x"></i></dt>
+                                <dd>phpmyadmin</dd> 
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+
+
+                {
+
+                /*
+                HTML :<i className="fab fa-html5 fa-3x"></i>
+                CSS : <i className="fab fa-css3-alt fa-3x"></i>
+                JS: <i className="fab fa-js-square fa-3x"></i>
+                SASS: <i className="fab fa-sass fa-3x"></i>
+                React : <i className="fab fa-react fa-3x"></i>
                 <br/> 
-                PHP: <i className="fab fa-php fa-6x"></i>
-                MySQL: : <i className="fas fa-database fa-6x"></i>
+                PHP: <i className="fab fa-php fa-3x"></i>
+                MySQL: : <i className="fas fa-database fa-3x"></i>
                 <br/>
-                BASH: <i className="fas fa-terminal fa-6x"></i>
-                GitHUB: <i className="fab fa-github fa-6x"></i>
-                
-                
+                BASH: <i className="fas fa-terminal fa-3x"></i>
+                GitHUB: <i className="fab fa-github fa-3x"></i>
+                */
+                }
             </div>
         </section>
     );
